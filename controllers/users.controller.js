3ae901c777088dfbf6f762cc752f485f18aff5f7
jack-laptop <jackie.duc.do@gmail.com>
2021-03-13 22:35:06 +0700

    edit

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -3,8 +3,6 @@ import expressValidator from 'express-validator'
 
 const getAllUsersAsync = async (req, res) => {
 	try {
-		// const conn = await connectDbAsync(req);
-		// const users = await userService.getAllUsersAsync(conn);
 		const users = await userService.getAllUsersAsync();
 		res.send(users);
 	}
@@ -16,8 +14,6 @@ const getAllUsersAsync = async (req, res) => {
 const addUserAsync = async (req, res) => {
 	try {
 		validateRequest(req);
-		// const conn = await connectDbAsync(req);
-		// const user = await userService.addUserAsync(conn, req.body);
 		const user = await userService.addUserAsync(req.body);
 		res.send(user);
 	} catch (error) {
@@ -28,8 +24,6 @@ const addUserAsync = async (req, res) => {
 const updateUserAsync = async (req, res) => {
 	try {
 		validateRequest(req);
-		// const conn = await connectDbAsync(req);
-		// const user = await userService.updateUserAsync(conn, req.body);
 		const user = await userService.updateUserAsync(req.body);
 		res.send(user);
 	} catch (error) {
@@ -40,27 +34,12 @@ const updateUserAsync = async (req, res) => {
 const deleteUserAsync = async (req, res) => {
 	try {
 		validateRequest(req);
-		// const conn = await connectDbAsync(req);
-		// const result = await userService.deleteUserAsync(conn, req.params.id);
 		const result = await userService.deleteUserAsync(req.params.id);
 		res.send(result);
 	} catch (error) {
 		res.status(500).send(error);
 	}
 }
-
-// const connectDbAsync = (request) => {
-// 	return new Promise((resolve, reject) => {
-// 		request.getConnection((err, conn) => {
-// 			if (!err) {
-// 				resolve(conn);
-// 			}
-// 			else {
-// 				reject({ message: err.message || "Something wrong happened" })
-// 			}
-// 		})
-// 	})
-// }
 
 const validateRequest = (req) => {
 	const errors = expressValidator.validationResult(req)?.array();
--- services/user.service.js
+++ services/user.service.js
@@ -3,40 +3,12 @@
 import User from '../models/user.model.js'
 
 
-// function getAllUsersAsync(conn) {
-//     return new Promise((resolve, reject) => {
-//         conn.query('SELECT * FROM users ORDER BY id DESC', function (err, rows) {
-//             if (err) {
-//                 reject({ message: err.message || "Something wrong happened" })
-//             }
-//             else {
-//                 resolve(rows)
-//             }
-//         })
-//     })
-// }
-
 async function getAllUsersAsync() {
     try {
         const users = await User.findAll();
         return users
     } catch (e) { throw e }
 }
-
-// function addUserAsync(conn, user) {
-//     return new Promise((resolve, reject) => {
-//         conn.query("INSERT INTO users SET ?", user, (err, result) => {
-//             if (err) {
-//                 reject({ message: err.message || "Something wrong happened" });
-//             }
-//             else {
-//                 console.log(result);
-//                 user.id = result.insertId;
-//                 resolve(user);
-//             }
-//         })
-//     })
-// }
 
 async function addUserAsync(user) {
     const { name, age, email } = user
@@ -47,22 +19,6 @@ async function addUserAsync(user) {
         return result
     } catch (e) { throw e }
 }
-
-// function updateUserAsync(conn, user) {
-//     return new Promise((resolve, reject) => {
-//         conn.query("UPDATE users set name = ?, email = ?, age = ? WHERE id = ?", [user.name, user.email, user.age, user.id], (err, result) => {
-//             if (err) {
-//                 reject({ message: err.message || "Something wrong happened" });
-//             }
-//             else if (!result.affectedRows) {
-//                 reject({ message: "User not found" });
-//             }
-//             else {
-//                 resolve(user);
-//             }
-//         })
-//     })
-// }
 
 async function updateUserAsync(user) {
     const { name, email, age, id } = user;
@@ -79,22 +35,6 @@ async function updateUserAsync(user) {
         }
     } catch (e) { throw e }
 }
-
-// function deleteUserAsync(conn, userId) {
-//     return new Promise((resolve, reject) => {
-//         conn.query("DELETE FROM users WHERE id = ?", [userId], (err, result) => {
-//             if (err) {
-//                 reject({ message: err.message || "Something wrong happened" });
-//             }
-//             else if (!result.affectedRows) {
-//                 reject({ message: "User not found" });
-//             }
-//             else {
-//                 resolve({ message: "User has been successfully deleted" });
-//             }
-//         })
-//     })
-// }
 
 async function deleteUserAsync(id) {
     try {
